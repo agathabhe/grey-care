33c8258f201cd790f674dac64519df0147beca97
Agatha Bhenares Alves Martins Rodrigues <agathabmartinsr@gmail.com>
2019-04-30 00:02:49 -0300

    Componentes e alterações

---
+++ components/menu/menu.vue
@@ -0,0 +1,22 @@
+<template id="menu">
+    <div class="col-sm-3  sidenav" style="padding-right: 15px">
+        <h2 style="color: white; margin-left: 10px"> Menu</h2>
+            <hr>
+            <ul class="nav nav-pills nav-stacked" style="margin-left: 10px; margin-bottom: 380px">
+                <li class="active"><a href="#" id="linkcolor" v-on:click="escondePacientes()">Página Inicial</a>
+                </li>
+                <li class="active"><a href="#" id="linkcolor" v-on:click="mostrarPacientes()">Pacientes</a></li>
+                <!--<li class="acitve"><a href="#" id="linkcolor" class="disabled">Consultas</a></li>-->
+            </ul>
+            <span style="margin-left: 2%">
+                <a class="disabled" id="linkcolor"><img src="../../Images/gears.png"
+                        style="width:30px; height: 30px"></img>Alterar meus dados</a>
+            </span>
+    </div>
+</template>
+
+<script>
+module.exports = {
+name: "menusidebar",
+};
+</script>
