c1c3a7323fede4a2363cd2af396aabec7fe62fc8
JohnhatanJ <48990742+JohnhatanJ@users.noreply.github.com>
2019-05-20 22:31:52 -0300

    Add files via upload
    Ainda sobre arrumar os problemas da sidebar

--- grey-care-client/components/menu/menu.vue
+++ grey-care-client/components/menu/menu.vue
@@ -1,18 +1,15 @@
 <template id="menu">
-    <div class="col-sm-3  sidenav" style="padding-right: 15%">
-        <h2 style="color: white; margin-left: 10px"> Menu</h2>
+    <nav class="col-sm-3  sidenav" style="padding-right: 1%">
+        <h2 style="color: white; margin-left: 3.5%"> Menu</h2>
             <hr>
-            <ul class="nav nav-pills nav-stacked" style="margin-left: 10px; margin-bottom: 380px">
+            <ul class="nav nav-pills nav-stacked" style="margin-left: 3%">
                 <li class="active"><a href="#" id="linkcolor" v-on:click="escondePacientes()">Página Inicial</a>
                 </li>
                 <li class="active"><a href="paciente-inserir.html" id="linkcolor" v-on:click="mostrarPacientes()">Pacientes</a></li>
+                <li class="active"><a class="disabled" id="linkcolor"><img src="Images/gears.png" style="width:30px; height: 30px"></img>Alterar meus dados</a></li>
                 <!-- <li class="acitve"><a href="paciente-inserir.html" id="linkcolor">Inserir pacientes</a></li> -->
             </ul>
-            <span style="margin-left: 2%">
-                <a class="user-editar.html" id="linkcolor"><img src="../../Images/gears.png"
-                        style="width:30px; height: 30px"/>Alterar meus dados</a>
-            </span>
-    </div>
+    </nav>
 </template>
 
 <script>
