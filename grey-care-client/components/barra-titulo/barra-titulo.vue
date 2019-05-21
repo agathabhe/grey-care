e49348449638f93c406b104dfc02ef82855ad720
Agatha Bhenares Alves Martins Rodrigues <agathabmartinsr@gmail.com>
2019-05-21 14:22:56 -0300

    Atualizando tela principal

--- grey-care-client/components/barra-titulo/barra-titulo.vue
+++ grey-care-client/components/barra-titulo/barra-titulo.vue
@@ -2,12 +2,10 @@
   <header id="mainheader">
     <p></p>
     <span id="cabecalho">
-      &nbsp; Seja bem vindo(a), {{nomeusuario}}!
-      <span style="margin-left: 35%">
-        <a href="#" @click="logout" id="buttonLink">
-          <button class="btn btn-danger my-2 my-sm-6" type="button" id="sair">Sair</button>
-        </a>
-      </span>
+      &nbsp; Seja bem vindo(a), {{nomeusuario}}! &nbsp; &nbsp; &nbsp;
+      <a href="#" @click="logout" id="buttonLink" >
+        <button class="btn btn-danger my-2 my-sm-6" type="button" id="sair">Sair</button>
+      </a>
     </span>
     <p style="margin-bottom: 15px"></p>
   </header>
--- grey-care-client/components/menu/menu.vue
+++ grey-care-client/components/menu/menu.vue
@@ -1,13 +1,13 @@
 <template id="menu">
     <nav class="col-sm-3  sidenav" style="padding-right: 1%">
-        <h2 style="color: white; margin-left: 3.5%"> Menu</h2>
-            <hr>
-            <ul class="nav nav-pills nav-stacked" style="margin-left: 3%">
+        <img src="Images/LogoGC.png" style="margin-left: 3.5%; width: 25%; margin-top: 1%"></img>
+            
+            
+            <ul class="nav nav-pills nav-stacked" style="margin-left: 3%; margin-top: 9%">
                 <li class="active"><a href="#" id="linkcolor" v-on:click="escondePacientes()">Página Inicial</a>
                 </li>
-                <li class="active"><a href="paciente-inserir.html" id="linkcolor" v-on:click="mostrarPacientes()">Pacientes</a></li>
+                <li class="active"><a href="paciente-inserir.html" id="linkcolor" v-on:click="mostrarPacientes()">Cadastrar paciente</a></li>
                 <li class="active"><a class="disabled" id="linkcolor"><img src="Images/gears.png" style="width:30px; height: 30px"></img>Alterar meus dados</a></li>
-                <!-- <li class="acitve"><a href="paciente-inserir.html" id="linkcolor">Inserir pacientes</a></li> -->
             </ul>
     </nav>
 </template>
