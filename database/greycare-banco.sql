8efb16674199429108dd8ae3bf1217172f0c7661
Agatha Bhenares Alves Martins Rodrigues <agathabmartinsr@gmail.com>
2019-06-24 17:10:34 -0300

    Adicionando script sql do banco

--- database/greycare-banco.sql
+++ database/greycare-banco.sql
@@ -5,7 +5,7 @@
 -- Dumped from database version 9.5.17
 -- Dumped by pg_dump version 9.5.17
 
--- Started on 2019-05-21 22:58:58 -03
+-- Started on 2019-06-24 17:08:38 -03
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
@@ -17,17 +17,18 @@ SET xmloption = content;
 SET client_min_messages = warning;
 SET row_security = off;
 
+DROP DATABASE "greycare-spring";
 --
--- TOC entry 2154 (class 1262 OID 16384)
--- Name: grey_care; Type: DATABASE; Schema: -; Owner: postgres
+-- TOC entry 2153 (class 1262 OID 16413)
+-- Name: greycare-spring; Type: DATABASE; Schema: -; Owner: postgres
 --
 
-CREATE DATABASE grey_care WITH TEMPLATE = template0 ENCODING = 'UTF8' LC_COLLATE = 'pt_BR.UTF-8' LC_CTYPE = 'pt_BR.UTF-8';
+CREATE DATABASE "greycare-spring" WITH TEMPLATE = template0 ENCODING = 'UTF8' LC_COLLATE = 'pt_BR.UTF-8' LC_CTYPE = 'pt_BR.UTF-8';
 
 
-ALTER DATABASE grey_care OWNER TO postgres;
+ALTER DATABASE "greycare-spring" OWNER TO postgres;
 
-\connect grey_care
+\connect -reuse-previous=on "dbname='greycare-spring'"
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
@@ -48,7 +49,7 @@ CREATE EXTENSION IF NOT EXISTS plpgsql WITH SCHEMA pg_catalog;
 
 
 --
--- TOC entry 2157 (class 0 OID 0)
+-- TOC entry 2156 (class 0 OID 0)
 -- Dependencies: 1
 -- Name: EXTENSION plpgsql; Type: COMMENT; Schema: -; Owner: 
 --
@@ -56,39 +57,12 @@ CREATE EXTENSION IF NOT EXISTS plpgsql WITH SCHEMA pg_catalog;
 COMMENT ON EXTENSION plpgsql IS 'PL/pgSQL procedural language';
 
 
-SET default_tablespace = '';
-
-SET default_with_oids = false;
-
 --
--- TOC entry 183 (class 1259 OID 16397)
--- Name: pacientes; Type: TABLE; Schema: public; Owner: postgres
+-- TOC entry 182 (class 1259 OID 16422)
+-- Name: hibernate_sequence; Type: SEQUENCE; Schema: public; Owner: postgres
 --
 
-CREATE TABLE public.pacientes (
-    id bigint NOT NULL,
-    nome text,
-    cpf text,
-    nascimento text,
-    estado_civil text,
-    endereco text,
-    cep text,
-    email text,
-    alergias text,
-    tipo_sanguineo text,
-    doador text,
-    telefone text
-);
-
-
-ALTER TABLE public.pacientes OWNER TO postgres;
-
---
--- TOC entry 184 (class 1259 OID 16403)
--- Name: paciente_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
---
-
-CREATE SEQUENCE public.paciente_id_seq
+CREATE SEQUENCE public.hibernate_sequence
     START WITH 1
     INCREMENT BY 1
     NO MINVALUE
@@ -96,132 +70,116 @@ CREATE SEQUENCE public.paciente_id_seq
     CACHE 1;
 
 
-ALTER TABLE public.paciente_id_seq OWNER TO postgres;
+ALTER TABLE public.hibernate_sequence OWNER TO postgres;
+
+SET default_tablespace = '';
+
+SET default_with_oids = false;
 
 --
--- TOC entry 2158 (class 0 OID 0)
--- Dependencies: 184
--- Name: paciente_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+-- TOC entry 181 (class 1259 OID 16414)
+-- Name: paciente; Type: TABLE; Schema: public; Owner: postgres
 --
 
-ALTER SEQUENCE public.paciente_id_seq OWNED BY public.pacientes.id;
+CREATE TABLE public.paciente (
+    id integer NOT NULL,
+    alergias text,
+    cep text,
+    cpf text,
+    doador text,
+    email text,
+    endereco text,
+    estado_civil text,
+    nascimento text,
+    nome text,
+    telefone text,
+    tipo_sanguineo text
+);
 
+
+ALTER TABLE public.paciente OWNER TO postgres;
 
 --
--- TOC entry 181 (class 1259 OID 16385)
+-- TOC entry 183 (class 1259 OID 16424)
 -- Name: users; Type: TABLE; Schema: public; Owner: postgres
 --
 
 CREATE TABLE public.users (
-    id bigint NOT NULL,
-    username text,
-    senha text,
-    tipo text,
-    nome text,
+    id integer NOT NULL,
+    cpf text,
     crm text,
-    telefone text,
-    especialidade text,
     email text,
+    especialidade text,
     nascimento text,
-    cpf text
+    nome text,
+    senha text,
+    telefone text,
+    tipo text,
+    username text
 );
 
 
 ALTER TABLE public.users OWNER TO postgres;
 
 --
--- TOC entry 182 (class 1259 OID 16388)
--- Name: users_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
---
-
-CREATE SEQUENCE public.users_id_seq
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    NO MAXVALUE
-    CACHE 1;
-
-
-ALTER TABLE public.users_id_seq OWNER TO postgres;
-
---
--- TOC entry 2159 (class 0 OID 0)
+-- TOC entry 2157 (class 0 OID 0)
 -- Dependencies: 182
--- Name: users_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+-- Name: hibernate_sequence; Type: SEQUENCE SET; Schema: public; Owner: postgres
 --
 
-ALTER SEQUENCE public.users_id_seq OWNED BY public.users.id;
-
-
---
--- TOC entry 2030 (class 2604 OID 16405)
--- Name: id; Type: DEFAULT; Schema: public; Owner: postgres
---
-
-ALTER TABLE ONLY public.pacientes ALTER COLUMN id SET DEFAULT nextval('public.paciente_id_seq'::regclass);
+SELECT pg_catalog.setval('public.hibernate_sequence', 27, true);
 
 
 --
--- TOC entry 2029 (class 2604 OID 16390)
--- Name: id; Type: DEFAULT; Schema: public; Owner: postgres
---
-
-ALTER TABLE ONLY public.users ALTER COLUMN id SET DEFAULT nextval('public.users_id_seq'::regclass);
-
-
---
--- TOC entry 2160 (class 0 OID 0)
--- Dependencies: 184
--- Name: paciente_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
---
-
-SELECT pg_catalog.setval('public.paciente_id_seq', 12, true);
-
-
---
--- TOC entry 2147 (class 0 OID 16397)
--- Dependencies: 183
--- Data for Name: pacientes; Type: TABLE DATA; Schema: public; Owner: postgres
---
-
-INSERT INTO public.pacientes (id, nome, cpf, nascimento, estado_civil, endereco, cep, email, alergias, tipo_sanguineo, doador, telefone) VALUES (5, 'AGATHA BHENARES ALVES MARTINS RODRIGUES''', '065.160.603-98''', '1999-04-26', 'solteiro', 'Rua Marechal Hermes, 116, A', '63700000', 'agathabmartinsr@gmail.com', 'sahdab', '2', 'sim', '88994527526');
-INSERT INTO public.pacientes (id, nome, cpf, nascimento, estado_civil, endereco, cep, email, alergias, tipo_sanguineo, doador, telefone) VALUES (6, 'Agnes Bhenazir Alves Martins Rodrigues', '1923712389312', '1994-09-11', 'solteiro', 'Rua Marechal Hermes, 116, A', '63700000', 'agathabmartinsr@gmail.com', 'poeira', '1', 'nao', '88994527526');
-INSERT INTO public.pacientes (id, nome, cpf, nascimento, estado_civil, endereco, cep, email, alergias, tipo_sanguineo, doador, telefone) VALUES (7, 'AGATHA BHENARES ALVES MARTINS RODRIGUES', '1728919', '1999-09-02', 'solteiro', 'Rua Marechal Hermes, 116, A', '63700000', 'agathabmartinsr@gmail.com', 'nada', '3', 'sim', '88994527526');
-INSERT INTO public.pacientes (id, nome, cpf, nascimento, estado_civil, endereco, cep, email, alergias, tipo_sanguineo, doador, telefone) VALUES (8, 'Igor Claudino', '1239201010', '1997-04-09', 'solteiro', 'Rua Marechal Hermes', '63700000', 'igorclaudino@gmail.com', 'asdda', '7', 'sim', '88994527526');
-INSERT INTO public.pacientes (id, nome, cpf, nascimento, estado_civil, endereco, cep, email, alergias, tipo_sanguineo, doador, telefone) VALUES (9, 'AGATHA BHENARES ALVES MARTINS RODRIGUES', '2323899824327', '1999-04-04', 'solteiro', 'Rua Marechal Hermes', '63700000', 'agathabmartinsr@gmail.com', 'NADA', '6', 'nao', '88994527526');
-INSERT INTO public.pacientes (id, nome, cpf, nascimento, estado_civil, endereco, cep, email, alergias, tipo_sanguineo, doador, telefone) VALUES (10, 'AGATHA BHENARES ALVES MARTINS RODRIGUES', '23456789', '1999-04-09', 'solteiro', 'Rua Marechal Hermes', '63700000', 'agathabmartinsr@gmail.com', 'nada', '3', 'nao', '88994527526');
-INSERT INTO public.pacientes (id, nome, cpf, nascimento, estado_civil, endereco, cep, email, alergias, tipo_sanguineo, doador, telefone) VALUES (11, 'AGATHA BHENARES ALVES MARTINS RODRIGUES', '0987654567', '1999-04-26', 'solteiro', 'Rua Marechal Hermes', '63700000', 'agathabmartinsr@gmail.com', 'nada', '7', 'nao', '88994527526');
-INSERT INTO public.pacientes (id, nome, cpf, nascimento, estado_civil, endereco, cep, email, alergias, tipo_sanguineo, doador, telefone) VALUES (12, 'AGATHA BHENARES ALVES MARTINS RODRIGUES', '098765', '1999-04-26', 'solteiro', 'Rua Marechal Hermes', '63700000', 'agathabmartinsr@gmail.com', 'sfd', '8', 'sim', '88994527526');
-
-
---
--- TOC entry 2145 (class 0 OID 16385)
+-- TOC entry 2145 (class 0 OID 16414)
 -- Dependencies: 181
+-- Data for Name: paciente; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+INSERT INTO public.paciente (id, alergias, cep, cpf, doador, email, endereco, estado_civil, nascimento, nome, telefone, tipo_sanguineo) VALUES (23, 'Nada', '63700000', '12345678977', 'nao', 'stessysoysa@gmail.com', 'Rua Almirante Tamandaré, 999, ap 1', 'solteiro', '1999-11-11', 'Antonia Stefanne Sousa Alves', '88994527526', '2');
+INSERT INTO public.paciente (id, alergias, cep, cpf, doador, email, endereco, estado_civil, nascimento, nome, telefone, tipo_sanguineo) VALUES (27, 'Nenhuma', '63700000', '09382938293', 'nao', 'almada@gmail.com', 'Rua B, 99', 'casado', '1994-09-09', 'Anderson Almada', '8599229911', '6');
+INSERT INTO public.paciente (id, alergias, cep, cpf, doador, email, endereco, estado_civil, nascimento, nome, telefone, tipo_sanguineo) VALUES (24, 'Não sei', '63700000', '06512345621', 'nao', 'agathabmartinsr@gmail.com', 'Rua Marechal Hermes, 116, A', 'solteiro', '1999-04-26', 'Agatha Bhenares Alves Martins Rodrigues', '88994527526', '7');
+INSERT INTO public.paciente (id, alergias, cep, cpf, doador, email, endereco, estado_civil, nascimento, nome, telefone, tipo_sanguineo) VALUES (25, 'Poeira', '63700000', '12345678345', 'nao', 'jonhnhtattan@gmail.com', 'Rua Fisk', 'solteiro', '1997-03-02', 'Jonhnhattan Jérome', '88998838392', '1');
+INSERT INTO public.paciente (id, alergias, cep, cpf, doador, email, endereco, estado_civil, nascimento, nome, telefone, tipo_sanguineo) VALUES (26, 'undefined', 'undefined', 'undefined', 'undefined', 'undefined', 'undefined', 'undefined', 'undefined', 'undefined', 'undefined', 'undefined');
+INSERT INTO public.paciente (id, alergias, cep, cpf, doador, email, endereco, estado_civil, nascimento, nome, telefone, tipo_sanguineo) VALUES (2, 'undefined', 'undefined', 'undefined', 'undefined', 'undefined', 'undefined', 'undefined', 'undefined', 'undefined', 'undefined', 'undefined');
+
+
+--
+-- TOC entry 2147 (class 0 OID 16424)
+-- Dependencies: 183
 -- Data for Name: users; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
-INSERT INTO public.users (id, username, senha, tipo, nome, crm, telefone, especialidade, email, nascimento, cpf) VALUES (2, 'yutydtkjh', 'gfhv', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
-INSERT INTO public.users (id, username, senha, tipo, nome, crm, telefone, especialidade, email, nascimento, cpf) VALUES (3, NULL, 'agathabhe', 'AGATHA BHENARES ALVES MARTINS RODRIGUES', '123456', '127189', '88994527526', 'clinico geral', 'agathabmartinsr@gmail.com', '1999-04-26', 'null');
-INSERT INTO public.users (id, username, senha, tipo, nome, crm, telefone, especialidade, email, nascimento, cpf) VALUES (4, NULL, NULL, 'undefined', NULL, NULL, 'undefined', NULL, 'undefined', 'undefined', 'undefined');
-INSERT INTO public.users (id, username, senha, tipo, nome, crm, telefone, especialidade, email, nascimento, cpf) VALUES (5, NULL, 'igor', 'Igor', '1234', '1239293', '88994527526', 'neurocirurgião', 'igor@mail.com', '1997-04-09', 'null');
-INSERT INTO public.users (id, username, senha, tipo, nome, crm, telefone, especialidade, email, nascimento, cpf) VALUES (6, NULL, 'grakt', 'igorkasjdka', '1234', '23091293819', '2738127389', 'sdakksdal', 'igor@gmail.com', '1997-04-09', 'null');
-INSERT INTO public.users (id, username, senha, tipo, nome, crm, telefone, especialidade, email, nascimento, cpf) VALUES (7, NULL, 'null', 'null', 'null', 'null', 'null', 'null', 'null', 'null', 'null');
-INSERT INTO public.users (id, username, senha, tipo, nome, crm, telefone, especialidade, email, nascimento, cpf) VALUES (8, 'secretario', 'dasdad', 'asdjkasdha', '123123', 'null', '123123', 'null', 'igor@mail.com', '1997-04-09', '12312');
-INSERT INTO public.users (id, username, senha, tipo, nome, crm, telefone, especialidade, email, nascimento, cpf) VALUES (9, 'medico', 'iudiu', 'iudiu filho', '123123', '98279', '8899999999', 'medico ofta', 'igor@mail.com', '1999-05-09', 'null');
-INSERT INTO public.users (id, username, senha, tipo, nome, crm, telefone, especialidade, email, nascimento, cpf) VALUES (10, 'agathabhe', '123123', 'secretario', 'AGATHA BHENARES ALVES MARTINS RODRIGUES', 'null', '88994527526', 'null', 'agathabmartinsr@gmail.com', '1999-04-26', '9876543456789');
+INSERT INTO public.users (id, cpf, crm, email, especialidade, nascimento, nome, senha, telefone, tipo, username) VALUES (1, 'null', '1231231', 'agathabmartinsr@gmail.com', 'asdnasd', '1999-04-26', 'AGATHA BHENARES ALVES MARTINS RODRIGUES', '123456', '88994527526', 'medico', 'agathabhe');
+INSERT INTO public.users (id, cpf, crm, email, especialidade, nascimento, nome, senha, telefone, tipo, username) VALUES (14, '1', 'null', 'agathabmartinsr@gmail.com', 'null', '1999-04-26', 'AGATHA BHENARES ALVES MARTINS RODRIGUES', '1234567', '88994527526', 'secretario', 'agathabhe');
+INSERT INTO public.users (id, cpf, crm, email, especialidade, nascimento, nome, senha, telefone, tipo, username) VALUES (15, '1', 'null', 'stessysousa@gmail.com', 'null', '1999-03-12', 'Antonia Stefanne Sousa Alves', '12345678', '88991212121', 'secretario', 'stessysousa');
+INSERT INTO public.users (id, cpf, crm, email, especialidade, nascimento, nome, senha, telefone, tipo, username) VALUES (19, '12345678908', 'null', 'igor@mail.com', 'null', '1997-04-09', 'Igor Claudino', '1234567', '98766544', 'secretario', 'igor_');
+INSERT INTO public.users (id, cpf, crm, email, especialidade, nascimento, nome, senha, telefone, tipo, username) VALUES (20, '87657865676', 'null', 'igor@mail.com', 'null', '1997-04-09', 'Igor Claudino', '123456', '98777667', 'secretario', 'igor_c');
+INSERT INTO public.users (id, cpf, crm, email, especialidade, nascimento, nome, senha, telefone, tipo, username) VALUES (21, '78909876787', 'null', 'igor@mail.com', 'null', '1997-04-09', 'Igor Claudino', '567765', '9887666677', 'secretario', 'igorc');
+INSERT INTO public.users (id, cpf, crm, email, especialidade, nascimento, nome, senha, telefone, tipo, username) VALUES (3, '99999999999', 'null', 'igor@mail.com.br', 'null', '1997-04-09', 'Igor Claudino', '654321', '88999999999', 'secretario', 'igor');
+INSERT INTO public.users (id, cpf, crm, email, especialidade, nascimento, nome, senha, telefone, tipo, username) VALUES (22, 'null', '123456', 'nenis@mail.com', 'Cirurgiã ', '1999-04-26', 'Nenis do Igor', '1234567', '919291921929', 'medico', 'nenis');
 
 
 --
--- TOC entry 2161 (class 0 OID 0)
--- Dependencies: 182
--- Name: users_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+-- TOC entry 2028 (class 2606 OID 16421)
+-- Name: paciente_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
-SELECT pg_catalog.setval('public.users_id_seq', 10, true);
+ALTER TABLE ONLY public.paciente
+    ADD CONSTRAINT paciente_pkey PRIMARY KEY (id);
 
 
 --
--- TOC entry 2156 (class 0 OID 0)
+-- TOC entry 2030 (class 2606 OID 16431)
+-- Name: users_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.users
+    ADD CONSTRAINT users_pkey PRIMARY KEY (id);
+
+
+--
+-- TOC entry 2155 (class 0 OID 0)
 -- Dependencies: 6
 -- Name: SCHEMA public; Type: ACL; Schema: -; Owner: postgres
 --
@@ -232,7 +190,7 @@ GRANT ALL ON SCHEMA public TO postgres;
 GRANT ALL ON SCHEMA public TO PUBLIC;
 
 
--- Completed on 2019-05-21 22:58:59 -03
+-- Completed on 2019-06-24 17:08:38 -03
 
 --
 -- PostgreSQL database dump complete
