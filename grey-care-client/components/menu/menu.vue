9790b3eb14460a00ed7c881528b72aae154185c4
JohnhatanJ <48990742+JohnhatanJ@users.noreply.github.com>
2019-06-23 21:25:29 -0300

    Modificação no menu.vue
    Modificado o Menu para acessar a todas as páginas do sistema.

--- grey-care-client/components/menu/menu.vue
+++ grey-care-client/components/menu/menu.vue
@@ -1,20 +1,14 @@
 <template id="menu">
     <nav class="col-sm-3 sidenav" style="padding-right: 1%">
-        <div style="margin-left: 3%"><h2>Menu</h2></div> 
+        <h2 style="color: white; margin-left: 3.5%"> Menu</h2>
         <hr>
             <ul class="nav nav-pills nav-stacked" style="margin-left: 3%; margin-top: 9%">
-                <li class="active"><a href="#" id="linkcolor" v-on:click="escondePacientes()">Página Inicial</a>
-                </li>
-                <li class="active"><a href="paciente-inserir.html" id="linkcolor" v-on:click="mostrarPacientes()">Cadastrar Paciente</a></li>
-                <li class="active"><a href="consulta-inserir.html" id="linkcolor" v-on:click="mostrarPacientes()">Cadastrar Consulta</a></li>
-                                    
-                  
-                  <span style="margin-left: 2%"><a class="user-editar.html" id="linkcolor"><img src="../../Images/gears.png"
-                        style="width:30px; height: 30px"></img>Alterar meus dados</a>
-                </span>              
+                <li class="active"><a href="#" id="linkcolor" v-on:click="escondePacientes()">Página Inicial</a></li>
+                <li class="active"><a href="#" id="linkcolor" v-on:click="mostrarPacientes()">Ver Pacientes</a></li>
+                <li class="active"><a href="paciente-inserir.html" id="linkcolor">Cadastrar Paciente</a></li>
+                <li class="active"><a href="alterar-dados.html" id="linkcolor"><img src="Images/gears.png" style="width:30px; height: 30px"></img>Alterar meus dados</a></li>
+                
             </ul>
-            
-
     </nav>
 </template>
 
