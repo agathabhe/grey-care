6d9875bb1c10f2c51634d673172c5d509e6fe234
Agatha Bhenares Alves Martins Rodrigues <agathabmartinsr@gmail.com>
2019-06-24 14:06:06 -0300

    Correcao do menu sidebar

--- grey-care-client/components/menu/menu.vue
+++ grey-care-client/components/menu/menu.vue
@@ -1,19 +1,59 @@
 <template id="menu">
-    <nav class="col-sm-3 sidenav" style="padding-right: 1%">
-        <h2 style="color: white; margin-left: 3.5%"> Menu</h2>
-        <hr>
-            <ul class="nav nav-pills nav-stacked" style="margin-left: 3%; margin-top: 9%">
-                <li class="active"><a href="#" id="linkcolor" v-on:click="mostrarPaginaInicial">Página Inicial</a></li>
-                <li class="active"><a href="#" id="linkcolor" v-on:click="mostrarPacientes()">Ver Pacientes</a></li>
-                <li class="active"><a href="paciente-inserir.html" id="linkcolor">Cadastrar Paciente</a></li>
-                <li class="active"><a href="#" id="linkcolor" v-on:click="mostrarUsuario(usuario)><img src="Images/gears.png" style="width:30px; height: 30px"></img>Alterar meus dados</a></li>
-                
-            </ul>
-    </nav>
+  <nav class="col-sm-3 sidenav" style="padding-right: 1%">
+    <h2 style="color: white; margin-left: 3.5%">Menu</h2>
+    <hr>
+    <ul class="nav nav-pills nav-stacked" style="margin-left: 3%; margin-top: 9%">
+      <li class="active">
+        <a href="#" id="linkcolor" v-on:click="mostrarPaginaInicial(page)">Página Inicial</a>
+      </li>
+      <li class="active">
+        <a href="#" id="linkcolor" v-on:click="mostrarPacientes(page)">Ver Pacientes</a>
+      </li>
+      <li class="active">
+        <a href="paciente-inserir.html" id="linkcolor">Cadastrar Paciente</a>
+      </li>
+      <li class="active">
+        <a href="#" id="linkcolor" v-on:click="mostrarUsuario(page)">
+          <img src="Images/gears.png" style="width:30px; height: 30px">Alterar meus dados
+        </a>
+      </li>
+    </ul>
+  </nav>
 </template>
 
 <script>
 module.exports = {
-name: "menusidebar",
+  name: "menusidebar",
+  props: ["page"],
+  methods: {
+    mostrarPacientes: function(page) {
+      page.mostraTabela = true;
+    },
+    mostrarPaginaInicial: function(page) {
+      page.mostraTabela = false;
+    },
+    mostrarUsuario: function(page) {
+    //   page.usuario = usuario;
+      //Recebe o Modal
+      var modal = document.getElementById("usuarioModal");
+
+      //Recebe a tag span que fecha o Modal
+      var span = document.getElementsByClassName("close")[0];
+
+      modal.style.display = "block";
+
+      //Fecha o Modal ao clicar no x
+      span.onclick = function() {
+        modal.style.display = "none";
+      };
+
+      //Quando clicar em qualquer lugar fora do Modal ele é fechado
+      window.onclick = function(event) {
+        if (event.target == modal) {
+          modal.style.display = "none";
+        }
+      };
+    }
+  }
 };
 </script>
